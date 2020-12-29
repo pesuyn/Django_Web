9be9dc3cd41b874105387b5ad5e0114bc47fca4f
Viet <martgold321@gmail.com>
2020-12-29 18:18:13 +0700

    Revert "dangkidangnhapdangxuat"
    This reverts commit 35b5c3e35b56f9ef85eb4a6f789858cec7dd8798.

--- KhachHang/admin.py
+++ KhachHang/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from KhachHang.models import product,bill,news,report,category,supplier,Profile,cart,label
+from KhachHang.models import product,bill,news,report,category,supplier,Profile,cart,label,Order, OrderItem
 # Register your models here.
 admin.site.register(product)
 admin.site.register(bill)
@@ -9,5 +9,9 @@ admin.site.register(category)
 admin.site.register(Profile)
 admin.site.register(cart)
 admin.site.register(label)
+admin.site.register(supplier)
+admin.site.register(OrderItem)
+admin.site.register(Order)
+
 
 
