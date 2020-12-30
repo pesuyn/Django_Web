726f0a74ca8e20da5973a82833ee33c288714d48
Viet <martgold321@gmail.com>
2020-12-30 21:38:25 +0700

    Sửa lại URL ĐK/ĐN

--- KhachHang/views.py
+++ KhachHang/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib import messages
 
@@ -25,15 +25,15 @@ class dangki(View):
     def post(self, request):
         # f = CreateUserForm()
         if request.method == 'POST':
-            hovaten = request.POST['hovaten']
-            name = request.POST['tendangnhap']
-            pw = request.POST['matkhau']
-            pw1 = request.POST['nhaplaimatkhau']
-            phone = request.POST['sodienthoai']
-            email = request.POST['email']
-            avatar = request.POST['avatar']
+            hovaten = request.POST.get('hovaten',False)
+            name = request.POST.get('tendangnhap', False)
+            pw = request.POST.get('matkhau', False)
+            pw1 = request.POST.get('nhaplaimatkhau', False)
+            phone = request.POST.get('sodienthoai', False)
+            email = request.POST.get('email', False)
+            avatar = request.POST.get('avatar', False)
             gioitinh = request.POST.get('gioitinh', False)
-            diachi = request.POST['diachi']
+            diachi = request.POST.get('diachi', False)
             alluser=User.objects.all()
             dem=0
 
@@ -61,7 +61,7 @@ class dangki(View):
                             my_group = Group.objects.get(name='Users')
                             my_group.user_set.add(user1)
                             messages.info(request, "Đăng kí thành công, hãy đăng nhập")
-                            return render(request, 'static/Khach_Hang/login.html')
+                            return redirect('trang-chu:dangnhap')
                 else:
                     messages.info(request, "Đăng kí thất bại, vui lòng nhập lại")
                     return render(request, 'static/Khach_Hang/register.html')
@@ -79,14 +79,15 @@ class dangnhap(View):
 
         if user is not None:
             login(request, user)
-            return render(request, 'static/Khach_Hang/TrangChu.html')
+            #return render(request, 'static/Khach_Hang/TrangChu.html')
+            return redirect('trang-chu:trangchu')
         else:
             messages.error(request, "Đăng nhập thất bại, nhập lại tài khoản")
             return render(request, 'static/Khach_Hang/login.html')
 
 def logoutUser(request):
     logout(request)
-    return render(request, 'static/Khach_Hang/TrangChu.html')
+    return redirect('trang-chu:trangchu')
 
 
 
